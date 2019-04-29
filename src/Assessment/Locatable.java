0b6056ce047094509d14b1eba4568f93cac924c2
finkelmanj7070 <finkelmanj7070@CA-SU-F106-25.sduhsd.lan>
2019-04-29 09:30:14 -0700

    April 23 School

---
+++ src/Assessment/Locatable.java
@@ -0,0 +1,15 @@
+package Assessment;
+
+
+public interface Locatable {
+
+    public void setPos(int x, int y);
+
+    public void setxPos(int x);
+
+    public void setyPos(int y);
+
+    public int getxPos();
+
+    public int getyPos();
+}
